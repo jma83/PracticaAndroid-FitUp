6d3af7dc80c3c12e3019a2ab09ca4c2b471183b1
javier.martinez <jma96.elx@gmail.com>
2021-06-09 20:32:09 +0200

    first commit

---
+++ app/src/main/java/es/upsa/mimo/v2021/fitup/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.upsa.mimo.v2021.fitup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
