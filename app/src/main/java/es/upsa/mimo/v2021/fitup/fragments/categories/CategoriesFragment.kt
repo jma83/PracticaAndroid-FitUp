9b3e2692fdab1c0492b78b92fabb608d14fbb14c
javier.martinez <jma96.elx@gmail.com>
2021-07-09 21:54:39 +0200

    Design improvements

--- app/src/main/java/es/upsa/mimo/v2021/fitup/fragments/categories/CategoriesFragment.kt
+++ app/src/main/java/es/upsa/mimo/v2021/fitup/fragments/categories/CategoriesFragment.kt
@@ -17,7 +17,7 @@ class CategoriesFragment: Fragment()  {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.activity_categories, container, false)
+        return inflater.inflate(R.layout.fragment_categories, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
